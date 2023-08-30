2420394b9bf8ecebcdcc23f22f1352a64ae5d188
ehsaanhridoy <ehsaanhridoy@gmail.com>
2023-08-30 23:57:10 +0600

    authentication,template added

---
+++ resources/views/backEnd/pages/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('backEnd.layouts.masters')
+
+@section('section-title', 'Dashboard')
+
+@section('content')
+
+<div class="row">
+    <div class="card">
+        <div class="card-header">Dashboard</div>
+    </div>
+</div>
+
+@endsection
